e52acbccaae421a580675eacb88fe2a58d67b975
asitparida <asitparida@live.in>
2017-02-16 17:05:39 +0530

    COmmit

--- QR.Web/src/QR.Web/wwwroot/app/author/controllers/app.controller.js
+++ QR.Web/src/QR.Web/wwwroot/app/author/controllers/app.controller.js
@@ -2,21 +2,30 @@
     "use strict";
     angular.module('QR.Web.Author')
     .controller('AppController', ['$uibModal', 'Notify', 'SharedService', function ($uibModal, notifyService, shared) {
-    var self = this;
-    self.shared = shared;
+        var self = this;
+        self.shared = shared;
 
-    self.notifyMsg = function () {
-        var data = {
-            message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore',
-            dismissable: false,
-            timer: 5000,
-            info: false,
-            success: false,
-            failed: false,
-            inProgress: true
-        };
-        notifyService.notify(data);
-    }
+        self.notifyMsg = function () {
+            var data = {
+                message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore',
+                dismissable: false,
+                timer: 5000,
+                info: false,
+                success: false,
+                failed: false,
+                inProgress: true
+            };
+            notifyService.notify(data);
+        }
 
-}]);
+        self.initJSWrapper = function (id) {
+            var flask = new CodeFlask;
+            flask.run('#' + id, { language: 'javascript' });
+            flask.onUpdate(function (code) {
+                var html = Prism.highlight(code, Prism.languages.javascript);
+                document.getElementById('displayPreview').innerHTML = html;
+            });
+        }
+
+    }]);
 })();
