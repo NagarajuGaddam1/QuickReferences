ff79c1738cae9b4f9397213840121c00f9f94bfb
asitparida <asitparida@live.in>
2017-02-24 18:09:50 +0530

    Refactoring Author App for adding states

--- QR.Web/src/QR.Web/wwwroot/app/author/configs/app.js
+++ QR.Web/src/QR.Web/wwwroot/app/author/configs/app.js
@@ -6,17 +6,39 @@
              $templateCache.put(tmpl, window._authorAssets[tmpl]);
          }
          window._authorAssets = null;
-     }])    
+     }])
 
     angular.module('QR.Web.Author', [
         'QR.Web.Extensions',
         'QR.Web.Author.templates',
-        'QR.Web.Author.Notify',        
+        'QR.Web.Author.Notify',
         'ngAnimate',
         'ngMaterial',
         'ui.bootstrap',
+        'ui.router',
         'ngSanitize',
         'ngDraggable'
-    ]);
+    ])
+    .config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state
+            ({
+                name: 'dashboard',
+                url: '/dashboard',
+                templateUrl: 'templates/author/author.dashboard.view.html',
+                controller: 'DashboardController',
+                controllerAs: 'authoringDashboard'
+            })
+            .state
+            ({
+                name: 'post',
+                url: '/post',
+                templateUrl: 'templates/author/author.post.view.html',
+                controller: 'PostController',
+                controllerAs: 'authoringPost'
+            });
+        $urlRouterProvider.otherwise('/post')
+    }])
+    .run();
 
 })();
---
+++ QR.Web/src/QR.Web/wwwroot/app/author/controllers/dashboard.controller.js
@@ -0,0 +1,9 @@
+﻿(function () {
+    "use strict";  
+
+    angular.module('QR.Web.Author')
+    .controller('DashboardController', ['$timeout', function ($timeout) {
+        var self = this;
+        self.timeout = $timeout;        
+    }]);
+})();
