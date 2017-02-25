a05f69581995e3bf1f81af2fe4fb068ab76fa502
Asit Parida <asitparida@live.in>
2017-02-25 21:15:06 +0530

    Adding authoring action controls

--- QR.Web/src/QR.Web/wwwroot/app/author/controllers/app.controller.js
+++ QR.Web/src/QR.Web/wwwroot/app/author/controllers/app.controller.js
@@ -2,9 +2,21 @@
     "use strict";
 
     angular.module('QR.Web.Author')
-    .controller('AppController', ['$timeout', 'SharedService', function ($timeout, SharedService) {
+    .controller('AppController', ['$timeout', 'SharedService','$state', function ($timeout, SharedService, $state) {
         var self = this;
         self.timeout = $timeout;
+        self.state = $state;
         self.shared = SharedService;
+        self.shared.actionClick = function(actionId){
+            console.log(actionId);
+            if(typeof self.shared[actionId] !== 'undefined' && typeof self.shared[actionId] === 'function')
+            {
+                self.shared[actionId]();
+            }
+        }
+        self.shared.goToDashboard = function()
+        {
+            self.state.go('dashboard');
+        }
     }]);
 })();
